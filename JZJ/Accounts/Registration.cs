65c61deedc981a53fbfbab07da4185015c82fc0b
j0kh4a <zukajoxa@gmail.com>
2026-05-01 23:58:45 +0400

    Add project files.

---
+++ JZJ/Accounts/Registration.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace JZJ.Accounts;
+
+internal class Registration
+{
+    public string Name { get; set; } = string.Empty;
+
+    public string LastName { get; set; } = string.Empty;
+
+    public string FullName { get => $"{Name} {LastName}"; }
+
+    public decimal Balance { get; set; } = 0;
+
+    public string Password { get; set; } = string.Empty;
+
+    public void ShowInfo()
+    {
+        Console.WriteLine($"saxeli da gavri: {FullName}");
+        Console.WriteLine($"balansis raodenoba: {Balance}");
+        Console.WriteLine($"paroli: {Password}");
+    }
+}
---
+++ JZJ/Program.cs
@@ -0,0 +1,64 @@
+﻿
+
+using JZJ.Accounts;
+
+
+Console.WriteLine("mogesalmebit JZJ-is bankomatshi");
+Registration registration = new Registration();
+
+
+string UserAction = string.Empty;
+string AdminAction = string.Empty;
+
+Console.Write("gtxovt airciot romeli xart 1.momxmarebeli / 2.administratori : ");
+string UserType = Console.ReadLine() ?? string.Empty;
+if (UserType == "1")
+{
+    Console.WriteLine("rit segvizlia dagexmarot dges?");
+    Console.Write("1.daregistrireba/2.sesvla /3.daxmareba : ");
+    UserAction = Console.ReadLine() ?? string.Empty;
+}
+else if (UserType == "2")
+{
+    Console.WriteLine("risi gaketeba gnebavt");
+    Console.Write("1.momxmareblebis siis naxva/2.sesxebis dadastureba : ");
+    AdminAction = Console.ReadLine() ?? string.Empty;
+}
+else
+{
+    Console.WriteLine("darwmundit rom romelime airciet");
+}
+
+if (UserAction == "1")
+{
+    Console.Write("gtoxvt seiyvanot tqveni saxeli : ");
+    registration.Name = Console.ReadLine() ?? string.Empty;
+
+    Console.Write("gtoxvt seiyvanot tqveni gvari : ");
+    registration.LastName = Console.ReadLine() ?? string.Empty;
+
+    Console.Write("gtoxvt seqmnat tqveni paroli: ");
+    registration.Password = Console.ReadLine() ?? string.Empty;
+
+    Console.Write("gtxovt seiyvanot ramdeni fuli semogaqvt : ");
+    registration.Balance = decimal.Parse(Console.ReadLine() ?? string.Empty);
+
+    Console.WriteLine();
+    registration.ShowInfo();
+}else if (UserAction == "2")
+{
+    Console.WriteLine("jer amis funcqia ar gvaqvs");
+}
+else if (UserAction == "3")
+{
+    Console.WriteLine("1.tu balansis semowmeba gindat jer equntshi unda sexvidet");
+    Console.WriteLine("2. tu sesxis ageba gnebavt sedit eqauntshi da smdegom moitxovet");
+    Console.WriteLine("3.tu raime tqenikuri xrsvezia sesxis agebastan balsntan and eqantant gtxovt dagvikavshirdet emailze " +
+        "jokhadze96@gmail.com / nikaZaqaidze@gmail.com/ nikaJabana@gmail.com" +
+        " an dagvikavshirdit sakontakto nomerze 996 67 67 67");
+}
+else
+{
+    Console.WriteLine("gtxovt darwumnit rom romelime airciet");
+}
+
